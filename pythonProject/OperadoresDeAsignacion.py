0bba8dac532738d25be1af42c44539eb7bc9ee9a
Jeferson Montenegro <jefersondavid3003@gmail.com>
2024-09-26 20:01:23 -0500

    se crea la carpeta de operadores de asignacion

---
+++ pythonProject/OperadoresDeAsignacion.py
@@ -0,0 +1,25 @@
+#operadores de acignacion
+var1 =5
+var2 =12
+var3 =8
+var4 =14
+var5 =6
+var6 =8
+
+var1 +=3  #var1 =var1+3 asignando con suma
+var2 -=3
+var3 *=3
+var4 /=3
+var5 %=3
+var6 //=3
+
+
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+
+

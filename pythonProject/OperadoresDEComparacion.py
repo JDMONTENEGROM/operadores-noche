f87834cdfbcbf3402c52dbc7c3f04aa7b3287fcb
Jeferson Montenegro <jefersondavid3003@gmail.com>
2024-09-26 20:54:46 -0500

    se agraga el archivo con operadores de comparacion

---
+++ pythonProject/OperadoresDEComparacion.py
@@ -0,0 +1,10 @@
+m =int (input("Ingrese el valor de m : "))
+n=int (input("Ingrese el valor de n : "))
+print(m==n) #igualdad
+print(m>n) #Mayor que
+print(m<n) #menor que
+print(m>=n) #Mayor igual que
+print(m<=n)
+print(m!=n) #no igual
+
+
